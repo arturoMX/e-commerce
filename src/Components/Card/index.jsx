1da7e183fcf7fb4673eda0bcd7bb66a5e870cf4e
Arturo Castro Ramirez <arturocr986@gmail.com>
2023-09-01 17:18:47 -0500

    Add API and get the API for show all products

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,16 +1,25 @@
-const Card = () => {
+import PropTypes from 'prop-types'
+
+const Card = ({price, title, images, category: {name}}) => {
+    Card.propTypes = {
+        price: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        images: PropTypes.array.isRequired,
+        category: PropTypes.object.isRequired,
+    }
+    
     return (
         <div className="bg-white cursor-pointer w-56 h-60 rounded-lg shadow-md">
             <figure className="relative mb-2 w-full h-4/5">
-                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">Electronics</span>
-                <img className="w-full h-full object-cover rounded-lg" src="https://images.pexels.com/photos/5939401/pexels-photo-5939401.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="heacphnoes" />
+                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{name}</span>
+                <img className="w-full h-full object-cover rounded-lg" src={images[0]} alt="heacphnoes" />
                 <button className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2">
                     +
                 </button>
             </figure>
             <p className="flex justify-between mx-2">
-                <span className="text-sm font-light">Headphones</span>
-                <span className="text-sm font-medium">$400</span>
+                <span className="text-sm font-light">{title}</span>
+                <span className="text-sm font-medium">${price}</span>
             </p>
         </div>
     )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,42 @@
+import { useState, useEffect } from "react";
 import { Card } from "../../Components/Card";
+import { apiUrl } from "../../API";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch(`${apiUrl}/products`)
+  //       const data = await response.json()
+  //       setItems(data)
+  //     } catch (error) {
+  //       console.error(`Oh no, ocurrió un error: ${error}`);
+  //     }
+  //   }
+  //   fetchData()
+  // }, [])
+
+  useEffect(() => {
+    fetch(`${apiUrl}/products`)
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
+
+
   return (
-    <Card />
+    <>
+    <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+    {
+      items?.map(item => (
+        // Destructuración
+        <Card key={item.id} {...item} />
+      ))
+    }
+    </div>
+    </>
   )
 }
 
