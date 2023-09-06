92b5e6a1016a029fef59b7efb61871db21aa51f4
Arturo Castro Ramirez <arturocr986@gmail.com>
2023-09-06 11:28:28 -0500

    Create a new component where can you view the product in a cart

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,4 +1,4 @@
-import { PropTypes } from "prop-types"
+import { PropTypes } from 'prop-types'
 import { TrashIcon } from '@heroicons/react/24/solid'
 
 const OrderCard = ({ title, images, price }) => {
